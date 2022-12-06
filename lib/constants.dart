288bbb8498d2ba24f70e363bd2fc28faf43a9c9b
apoorvsingh2000 <apoorvsinghlko@gmail.com>
2022-12-06 16:52:46 +1100

    scan screen started

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+//Text
+const kScanText = Text('Scan');
---
+++ lib/screens/scan_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:smart_bag/constants.dart';
+
+class ScanScreen extends StatefulWidget {
+  static const String id = 'scan_screen';
+  const ScanScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: TextButton(
+            onPressed: () {},
+            child: kScanText,
+          ),
+        ),
+      ),
+    );
+  }
+}
